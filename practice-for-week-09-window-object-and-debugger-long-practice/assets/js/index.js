aba6066608c574842b3d0d406a74e1e1f6e68cc6
jagLas <jglasher@gmail.com>
2023-03-09 14:45:48 -0500

    phase 1-2 done

--- practice-for-week-09-window-object-and-debugger-long-practice/assets/js/index.js
+++ practice-for-week-09-window-object-and-debugger-long-practice/assets/js/index.js
@@ -1,3 +1,17 @@
+import {
+    changeTitle,
+    changeHeader,
+    changeAboutMe
+} from "./manipulate.js"
+
+import { findElementById,
+    findElementsOfClass,
+    findElementsOfTag,
+    findFirstElementOfClass,
+    findFirstElementOfTag
+} from "./search.js"
+
+window.onload = () => {
 changeTitle();
 changeHeader();
 changeAboutMe();
@@ -6,3 +20,4 @@ console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</l
 console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
 console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
 console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+}

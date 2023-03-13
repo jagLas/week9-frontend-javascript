652b1de9d0b30ae73cae4787215d809bd28908c9
jagLas <jglasher@gmail.com>
2023-03-13 14:28:49 -0400

    practice done

--- practice-for-week-09-data-on-elements/data.js
+++ practice-for-week-09-data-on-elements/data.js
@@ -1,1 +1,21 @@
 // Your code here
+window.addEventListener('DOMContentLoaded', () => {
+    const add = document.querySelector('#add');
+    add.addEventListener('click', addToList);
+    console.log('eventmade')
+})
+
+function addToList(event){
+    // const form = document.querySelector('form')
+    event.preventDefault()
+    const name = document.querySelector('#name');
+    const type = document.querySelector('#type').value;
+    const item = document.createElement('li');
+    // debugger
+    item.innerText = name.value;
+    item.setAttribute('data-type', type)
+    name.value = ''
+    // debugger
+    document.querySelector('#shopping-list').appendChild(item);
+    console.log(item.dataset.type)
+}

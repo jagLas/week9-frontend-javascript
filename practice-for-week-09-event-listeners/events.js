003b003ee29674e8a0aeced79ec653e577c275ae
jagLas <jglasher@gmail.com>
2023-03-13 12:03:44 -0400

    first three done

--- practice-for-week-09-event-listeners/events.js
+++ practice-for-week-09-event-listeners/events.js
@@ -1,1 +1,26 @@
 // Your code here
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('The Dom has loaded');
+})
+
+window.addEventListener('DOMContentLoaded', ()=> {
+    const redInput = document.getElementById('red-input');
+    redInput.addEventListener('input', () => {
+        if (redInput.value === 'red') {
+            redInput.setAttribute('style', 'background-color: red')
+        } else {
+            redInput.setAttribute('style', 'background-color: transparent')
+        }
+    })
+})
+
+window.addEventListener('DOMContentLoaded', ()=> {
+    const addItemButton = document.getElementById('add-item');
+    addItemButton.addEventListener('click', () => {
+        const list = document.querySelector('#section-2 > ul')
+        const li = document.createElement('li');
+        const input = document.getElementById('list-add').value;
+        li.innerText = input;
+        list.appendChild(li);
+    })
+})

424304e497771d5ce39f33a8002d9304e098a1be
jagLas <jglasher@gmail.com>
2023-03-13 13:24:49 -0400

    remove event listeners

--- practice-for-week-09-event-listeners/events.js
+++ practice-for-week-09-event-listeners/events.js
@@ -1,34 +1,43 @@
 // Your code here
 window.addEventListener('DOMContentLoaded', () => {
     console.log('The Dom has loaded');
-})
-
-window.addEventListener('DOMContentLoaded', ()=> {
+    
     const redInput = document.getElementById('red-input');
-    redInput.addEventListener('input', function redListener() {
-        if (redInput.value === 'red') {
-            redInput.setAttribute('style', 'background-color: red')
-        } else {
-            redInput.setAttribute('style', 'background-color: transparent')
-        }
-    })
-})
+    redInput.addEventListener('input', redListener)
 
-window.addEventListener('DOMContentLoaded', ()=> {
     const addItemButton = document.getElementById('add-item');
-    addItemButton.addEventListener('click', () => {
-        const list = document.querySelector('#section-2 > ul')
-        const li = document.createElement('li');
-        const input = document.getElementById('list-add').value;
-        li.innerText = input;
-        list.appendChild(li);
+    addItemButton.addEventListener('click', addItemListener)
+
+    const colorWheel = document.querySelector('#color-select');
+    colorWheel.addEventListener('change', colorWheelListener)
+
+    const removeButton = document.querySelector('#remove-listeners')
+    removeButton.addEventListener('click', () => {
+        redInput.removeEventListener('input', redListener)
+        addItemButton.removeEventListener('click', addItemListener)
+        colorWheel.removeEventListener('change', colorWheelListener)
     })
 })
 
-window.addEventListener('DOMContentLoaded', () => {
-    const colorWheel = document.querySelector('#color-select');
+function redListener(event) {
+    console.log(event.target.value)
+    if (event.target.value.toLowerCase() === 'red') {
+        event.target.setAttribute('style', 'background-color: red')
+    } else {
+        event.target.setAttribute('style', 'background-color: transparent')
+    }
+}
+
+function addItemListener(event) {
+    const list = document.querySelector('#section-2 > ul')
+    const li = document.createElement('li');
+    const input = document.getElementById('list-add').value;
+    li.innerText = input;
+    list.appendChild(li);
+}
+
+function colorWheelListener (event) {
     const div = document.querySelector('#section-3')
-    colorWheel.addEventListener('change', () => {
-        div.setAttribute('style', `background-color: ${colorWheel.value}`)
-    })
-})
+    div.setAttribute('style', `background-color: ${event.target.value}`)
+
+}

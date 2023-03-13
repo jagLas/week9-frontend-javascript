e367115d6ce58d33939aa96dd7f21f90b9c9f720
jagLas <jglasher@gmail.com>
2023-03-13 10:37:00 -0400

    phase 1a

--- element-manipulation-long-practice/my-profile.js
+++ element-manipulation-long-practice/my-profile.js
@@ -1,4 +1,26 @@
 
 window.onload = () => {
     console.log('this is my profile page')
+    document.body.innerHTML = `
+    <h1>James</h1>
+    `
+
+    let newUl = document.createElement('ul');
+    document.body.appendChild(newUl)
+
+    let newLi = document.createElement('li');
+    newLi.innerText = `I'm learning how to code`;
+    newUl.appendChild(newLi);
+ 
+    newLi = document.createElement('li');
+    newLi.innerText = `This page is to practice adding html via javscript`;
+    newUl.appendChild(newLi);
+
+    newLi = document.createElement('li');
+    newLi.innerText = `I'm looking forward to this exciting change in careers`;
+    newUl.appendChild(newLi);
+
+    newLi = document.createElement('li');
+    newLi.innerText = `Enjoy the page`;
+    newUl.appendChild(newLi);
 }

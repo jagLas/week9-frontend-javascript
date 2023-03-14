8ff6f92589462137c8b942aedc72a65c261b7b6b
jagLas <jglasher@gmail.com>
2023-03-13 20:05:43 -0400

    board creation added

--- practice-for-week-09-battleship-dom/assets/js/index.js
+++ practice-for-week-09-battleship-dom/assets/js/index.js
@@ -7,3 +7,20 @@ let board = new Board(); // creates a new game board
 console.log(board.grid);
 
 // Your code here
+function makeBoard () {
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('id', 'game-board')
+    console.log('making board')
+    board.grid.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const square = document.createElement('div');
+            square.setAttribute('data-row', rowI);
+            square.setAttribute('data-col', colI);
+            square.setAttribute('class', 'square')
+            wrapper.appendChild(square)
+        })
+    })
+    document.body.appendChild(wrapper);
+}
+
+window.onload = makeBoard;

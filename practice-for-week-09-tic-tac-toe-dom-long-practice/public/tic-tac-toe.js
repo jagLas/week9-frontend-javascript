648cba258cb45f82edd9e465943e9f6bfd39e056
jagLas <jglasher@gmail.com>
2023-03-16 15:12:20 -0400

    passes spec on test 2

--- practice-for-week-09-tic-tac-toe-dom-long-practice/public/tic-tac-toe.js
+++ practice-for-week-09-tic-tac-toe-dom-long-practice/public/tic-tac-toe.js
@@ -11,17 +11,21 @@ function clickListener () {
 }
 
 function placeMove(e) {
-    // debugger
+
     let square = e.target;
+    //prevents a TypeError if clicking on space that already is filled
+    if(square.tagName === 'IMG') {
+        return
+    }
+
     let id = square.id.split('-')[1];
     let row = Math.floor(id / 3);
     let col = id % 3;
-    console.log(col)
-    // console.log(game.grid[row])
-    // console.log(game.grid[row][col])
 
     if (grid[row][col] === ' '){
-        square.innerHTML = `<img src="./player-${game.playerTurn.toLowerCase()}.svg" alt="${game.playerTurn.toLowerCase()}">`
+        //the following code was originally made and worked, but failed cypress test due to specificity. If asset is ever removed from app academy, uncomment lower line and comment the line below that
+        // square.innerHTML = `<img src="./player-${game.playerTurn.toLowerCase()}.svg" alt="${game.playerTurn.toLowerCase()}">`
+        square.innerHTML = `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${game.playerTurn.toLowerCase()}.svg" alt="${game.playerTurn.toLowerCase()}">`
         console.log('placing move at', row, col)
         //updates the backend game state and checks for a winner
         game.placeMove(row, col);

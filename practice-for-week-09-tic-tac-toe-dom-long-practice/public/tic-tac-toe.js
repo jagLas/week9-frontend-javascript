3dc3dae2461dcfd216dbae86ddf3ffbddbbeb7ea
jagLas <jglasher@gmail.com>
2023-03-16 19:50:13 -0400

    simplified code

--- practice-for-week-09-tic-tac-toe-dom-long-practice/public/tic-tac-toe.js
+++ practice-for-week-09-tic-tac-toe-dom-long-practice/public/tic-tac-toe.js
@@ -11,41 +11,40 @@ function clickListener () {
 }
 
 function placeMove(e) {
-
     let square = e.target;
+
     //prevents a TypeError if clicking on space that already is filled
     if(square.tagName === 'IMG') {
-        return
+        console.log('Unable to place move: space occupied');
+        return;
     }
 
+    //turns id into coordinates for 2d matrix
     let id = square.id.split('-')[1];
     let row = Math.floor(id / 3);
     let col = id % 3;
 
-    if (grid[row][col] === ' '){
-        //the following code was originally made and worked, but failed cypress test due to specificity. If asset is ever removed from app academy, uncomment lower line and comment the line below that
-        // square.innerHTML = `<img src="./player-${game.playerTurn.toLowerCase()}.svg" alt="${game.playerTurn.toLowerCase()}">`
-        square.innerHTML = `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${game.playerTurn.toLowerCase()}.svg" alt="${game.playerTurn.toLowerCase()}">`
-        console.log('placing move at', row, col)
-        //updates the backend game state and checks for a winner
-        game.placeMove(row, col);
-        let winner = TTT.checkWin(grid);
-        if (winner) onWin(winner);
+    //updates the ui to show move
+    // the following code was originally made and worked, but failed cypress test due to specificity. If asset is ever removed from app academy, uncomment lower line and comment the line below that
+    square.innerHTML = `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${game.playerTurn.toLowerCase()}.svg" alt="${game.playerTurn.toLowerCase()}">`
+    // square.innerHTML = `<img src="./player-${game.playerTurn.toLowerCase()}.svg" alt="${game.playerTurn.toLowerCase()}">`
+    
+    // console.log('placing move at', row, col)
 
-    } else {
-        console.log('unable to place move at', row, col);
-    }
+    //updates the backend game state and checks for a winner
+    game.placeMove(row, col);
+    let winner = TTT.checkWin(grid);
+    if (winner) onWin(winner);
 
-    console.log(grid)
+    // console.log(grid)
 }
 
 function onWin(winner) {
-    console.log(winner)
+    // console.log(winner)
     const winSpan = document.querySelector('#winner')
     winSpan.innerText = winner;
     const board = document.querySelector('#board');
     board.removeEventListener('click', placeMove)
-
 }
 
 window.onload = () => {
